425a76a7e36e44677ad28c8c10917c38345d4467
Security Theater <moeteam2016@gmail.com>
2018-10-11 20:42:13 +0200

    Update 2018_08_10_220150_create_market_place_table.php

--- src/Publishables/migrations/2018_08_10_220150_create_market_place_table.php
+++ src/Publishables/migrations/2018_08_10_220150_create_market_place_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMarketFluentTable extends Migration
+class CreateMarketPlaceTable extends Migration
 {
     /**
      * Run the migrations.
